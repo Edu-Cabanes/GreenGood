511188809d77c6cdef023a6ff3d94dec07eeb38d
EduCabanes <educabanes54@gmail.com>
2026-03-26 17:20:28 +0100

    chore: initial project structure with Vite

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>GreenGood</h1>
+      <p>¡Bienvenido! El proyecto está listo para rodar.</p>
+    </div>
+  )
+}
+
+export default App
